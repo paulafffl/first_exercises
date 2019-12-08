ec68fe37d9b3ff176a5a2488d89cd8c0e6aa5404
Paula <paulafffl@gmail.com>
2019-12-08 10:04:40 +0000

    Creating first door div, style and interactivity

---
+++ ChoreDoor/resources/script.js
@@ -0,0 +1,5 @@
+let doorimage1 = document.getElementById('door1');
+const botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
+
+
+doorImage1.onclick = () => doorImage1.src = botDoorPath;

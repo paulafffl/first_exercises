b799d1367f8878c27e38d8b761f8ae851418f44c
Paula <paulafffl@gmail.com>
2019-12-08 21:05:32 +0000

    Created function for the doors to be sorted randomly

--- ChoreDoor/resources/script.js
+++ ChoreDoor/resources/script.js
@@ -6,6 +6,30 @@ const botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-
 const beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
 const spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
 
-doorImage1.onclick = () => doorImage1.src = botDoorPath;
-doorImage2.onclick = () => doorImage2.src = beachDoorPath;
-doorImage3.onclick = () => doorImage3.src = spaceDoorPath;
+const numClosedDoors = 3;
+let openDoor1;
+let openDoor2;
+let openDoor3;
+
+randomChoreDoorGenerator = () => {
+  const choreDoor = Math.floor(Math.random()*numClosedDoors);
+  if (choreDoor === 0){
+    openDoor1 = botDoorPath;
+    openDoor2 = beachDoorPath;
+    openDoor3 = spaceDoorPath;
+  } else if (choreDoor === 1){
+    openDoor1 = spaceDoorPath;
+    openDoor2 = botDoorPath;
+    openDoor3 = beachDoorPath;
+  } else { (choreDoor === 2)
+    openDoor1 = beachDoorPath;
+    openDoor2 = spaceDoorPath;
+    openDoor3 = botDoorPath;
+  }
+}
+
+doorImage1.onclick = () => doorImage1.src = openDoor1;
+doorImage2.onclick = () => doorImage2.src = openDoor2;
+doorImage3.onclick = () => doorImage3.src = openDoor3;
+
+randomChoreDoorGenerator();

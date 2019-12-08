48461c43da86d249113413fc3a2685e91043070a
Paula <paulafffl@gmail.com>
2019-12-08 10:33:41 +0000

    Creating second and third doors

--- ChoreDoor/resources/script.js
+++ ChoreDoor/resources/script.js
@@ -1,5 +1,11 @@
-let doorimage1 = document.getElementById('door1');
-const botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
+let doorImage1 = document.getElementById('door1');
+let doorImage2 = document.getElementById('door2');
+let doorImage3 = document.getElementById('door3');
 
+const botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
+const beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
+const spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
 
 doorImage1.onclick = () => doorImage1.src = botDoorPath;
+doorImage2.onclick = () => doorImage2.src = beachDoorPath;
+doorImage3.onclick = () => doorImage3.src = spaceDoorPath;
